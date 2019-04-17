ecd38ec8ff3c516e7669c03fd7832ba81c8d95e3
Neville <neville.walo@gmail.com>
2019-04-17 20:36:27 +0200

    added prime functions

--- main.cpp
+++ main.cpp
@@ -1,38 +1,86 @@
 #include <iostream>
 #include <math.h>
+#include <chrono>
+#include <functional>
 
-// function to add the elements of two arrays
-void add(int n, float *x, float *y)
-{
-    for (int i = 0; i < n; i++)
-        y[i] = x[i] + y[i];
+double Runtime(const std::function<void(void)> &f) {
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    std::chrono::duration<double, std::ratio<1>> duration;
+    int repetitions = 10;
+    double sum = .0;
+
+    for (int i = 0; i < repetitions; ++i) {
+        start = std::chrono::high_resolution_clock::now();
+        f();
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        sum += (double) duration.count();
+    }
+
+    return sum / repetitions;
 }
 
-int main(void)
-{
-    int N = 1<<20; // 1M elements
+bool isPrime(int num) {
+    bool flag = true;
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
 
-    float *x = new float[N];
-    float *y = new float[N];
+// function to add the elements of two arrays
+void f(int n, int *x, bool *y) {
+    for (int i = 0; i < n; i++)
+        y[i] = isPrime(x[i]);
+}
+
+int main(void) {
+    int N = 1 << 5; // 1M elements
+//    int N = 1 << 20; // 1M elements
+
+    int *x = new int[N];
+    bool *y = new bool[N];
+
 
     // initialize x and y arrays on the host
     for (int i = 0; i < N; i++) {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
+        x[i] = i % 100;
+
     }
 
     // Run kernel on 1M elements on the CPU
-    add(N, x, y);
+    std::function<void(void)> benchmark = [&x, &y, &N]() {
+        f(N, x, y);
+    };
 
+    double runtime = Runtime(benchmark);
+
+    std::cout << "Runtime: " << runtime * 1000 << std::endl;
+    std::cout << "Array x: " << *x << std::endl;
+    std::cout << "Array y: " << *y << std::endl;
+
+    std::cout << "Array x: ";
+    for (int j = 0; j < N; ++j) {
+        std::cout << x[j] << ",";
+    }
+
+    std::cout << "\n";
+    std::cout << "Array y: ";
+    for (int j = 0; j < N; ++j) {
+        std::cout << y[j] << ",";
+    }
     // Check for errors (all values should be 3.0f)
-    float maxError = 0.0f;
-    for (int i = 0; i < N; i++)
-        maxError = fmax(maxError, fabs(y[i]-3.0f));
-    std::cout << "Max error: " << maxError << std::endl;
+//        float maxError = 0.0f;
+//        for (int i = 0; i < N; i++)
+//            maxError = fmax(maxError, fabs(y[i] - 12.0f));
+//        std::cout << "Max error: " << maxError << std::endl;
 
     // Free memory
-    delete [] x;
-    delete [] y;
+    delete[] x;
+    delete[] y;
 
     return 0;
 }
